226bc97cf4fd16620ea5dd595a589bcd40e530cb
Brian Shu <littlebubu.shu@gmail.com>
2020-08-06 23:22:29 -0400

    added tests and editor open dir listing functionality

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod exit_codes;
 mod filesystem;
 mod opt;
 
-use std::env::{self, temp_dir};
+use std::env;
 use std::fs::{self, File};
 use std::io::{Read, Write};
 use std::path::Path;
@@ -10,39 +10,60 @@ use std::process::{self, Command};
 
 use anyhow::{anyhow, Context, Result};
 use rayon::prelude::*;
+use structopt::StructOpt;
 use tempfile::NamedTempFile;
 
 use exit_codes::ExitCode;
+use filesystem::{convert_paths_to_string, get_sorted_paths};
 use opt::Opt;
 
-fn try_main() -> Result<()> {
+fn try_main(opt: Opt) -> Result<()> {
     let editor = match env::var("EDITOR") {
         Ok(editor) => editor,
         Err(_) => String::from("vim"),
     };
 
-    let file = NamedTempFile::new().context("Failed to create named tempfile")?;
+    let mut file = NamedTempFile::new().context("Failed to create named tempfile")?;
+
+    let paths = get_sorted_paths("./")?;
+    write!(
+        file,
+        "{}",
+        convert_paths_to_string(paths)
+            .collect::<Result<Vec<String>>>()?
+            .join("\n")
+    )
+    .context(format!(
+        "Failed to write to temp file {}",
+        file.path().display()
+    ))?;
+
     let file_path = file.path();
 
-    let mut child = Command::new(&editor)
-        .arg(&file_path)
-        .spawn()
-        .context(format!(
-            "Failed to open {} with {}.
+    if let Some(pattern) = opt.pattern {
+        let rename = opt.rename.context("--rename must be supplied if you are renaming with --pattern ")?;
+    } else {
+        let mut child = Command::new(&editor)
+            .arg(&file_path)
+            .spawn()
+            .context(format!(
+                "Failed to open {} with {}.
 
-Bulkrename will default to vim if $EDITOR is not set.
-Please check if vim or $EDITOR exists on your system.",
-            &file_path.display(),
-            editor
-        ))?;
+    Bulkrename will default to vim if $EDITOR is not set.
+    Please check if vim or $EDITOR exists on your system.",
+                &file_path.display(),
+                editor
+            ))?;
 
-    let ecode = child
-        .wait()
-        .context(format!("Failed to wait on child command {}", &editor))?;
-    if !ecode.success() {
-        return Err(anyhow!(
-            "{} exited with a non-zero code. Not changing any files."
-        ));
+        let ecode = child
+            .wait()
+            .context(format!("Failed to wait on child command {}", &editor))?;
+
+        if !ecode.success() {
+            return Err(anyhow!(
+                "{} exited with a non-zero code. Not changing any files."
+            ));
+        }
     }
 
     let contents = fs::read_to_string(file_path)?;
@@ -52,10 +73,17 @@ Please check if vim or $EDITOR exists on your system.",
 }
 
 fn main() {
-    match try_main() {
+    let opt = Opt::from_args();
+    let debug_mode = opt.debug;
+
+    match try_main(opt) {
         Ok(()) => process::exit(ExitCode::Success.into()),
         Err(e) => {
-            eprintln!("[bulk-rename error]: {}", e);
+            if debug_mode {
+                eprintln!("[bulk-rename error]: {:?}", e);
+            } else {
+                eprintln!("[bulk-rename error]: {}", e);
+            }
             process::exit(ExitCode::GeneralError.into())
         }
     }
